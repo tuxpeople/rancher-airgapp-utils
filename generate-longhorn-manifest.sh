ecdc67210f534c7c0dde8897a3797de8683317a2
Thomas Deutsch <thomas@tuxpeople.org>
2026-05-20 12:10:09 +0200

    add longhorn

---
+++ generate-longhorn-manifest.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+LONGHORN_VERSION=1.10.2
+#WORKDIR="/opt/hauler/rancher"
+WORKDIR="${WORKDIR:-${HOME}/Downloads/hauler/automate}"
+
+# Setup Working Directory
+rm -rf ${WORKDIR}
+mkdir -p ${WORKDIR}
+cd ${WORKDIR}
+
+# Get longhorn Images
+LONGHORN_IMAGES=$(curl -sSfL https://raw.githubusercontent.com/longhorn/longhorn/v${LONGHORN_VERSION}/deploy/longhorn-images.txt | sed -e "s/^/    - name: /")
+
+# Create Hauler Manifest
+cat > ${WORKDIR}/longhorn-manifest.yaml << EOF
+apiVersion: content.hauler.cattle.io/v1
+kind: Charts
+metadata:
+  name: longhorn-chart
+spec:
+  charts:
+    - name: longhorn
+      repoURL: https://charts.longhorn.io
+      version: ${LONGHORN_VERSION}
+---
+apiVersion: content.hauler.cattle.io/v1
+kind: Images
+metadata:
+  name: longhorn-images
+spec:
+  images:
+${LONGHORN_IMAGES}
+EOF
