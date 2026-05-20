65c973bf6f05ca9f31a02d37cdce4018b2d426e0
Thomas Deutsch <thomas@tuxpeople.org>
2026-05-20 11:36:59 +0200

    initial commit

---
+++ generate-certmanager-manifest.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+CERTMANAGER_VERSION=v1.19.0
+#WORKDIR="/opt/hauler/rancher"
+WORKDIR="${WORKDIR:-${HOME}/Downloads/hauler/automate}"
+
+# Setup Working Directory
+rm -rf ${WORKDIR}
+mkdir -p ${WORKDIR}
+cd ${WORKDIR}
+
+# Get Cert-Manager Images
+helm repo add jetstack https://charts.jetstack.io && helm repo update
+CERTMANAGER_IMAGES=$(helm template jetstack/cert-manager --version=${CERTMANAGER_VERSION} --include-crds | grep 'image:' | sed 's/"//g' | awk '{ print $2 }' | sed -e "s/^/    - name: /")
+
+# Create Hauler Manifest
+cat > ${WORKDIR}/certmanager-manifest.yaml << EOF
+apiVersion: content.hauler.cattle.io/v1
+kind: Charts
+metadata:
+  name: cert-manager-chart
+spec:
+  charts:
+    - name: cert-manager
+      repoURL: https://charts.jetstack.io
+      version: ${CERTMANAGER_VERSION}
+---
+apiVersion: content.hauler.cattle.io/v1
+kind: Images
+metadata:
+  name: cert-manager-images
+spec:
+  images:
+${CERTMANAGER_IMAGES}
+EOF
